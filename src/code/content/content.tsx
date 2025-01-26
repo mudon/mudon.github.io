78cb7f1f36707117593b630f67512223db01d51e
hazim <muhammadhazim@yahoo.com>
2025-01-26 21:20:17 +0800

    new

--- src/code/content/content.tsx
+++ src/code/content/content.tsx
@@ -8,6 +8,10 @@ import img2 from "../../assets/engaged/photo_2025-01-07_20-59-48.jpg";
 import img3 from '../../assets/engaged/photo_2025-01-07_20-59-50.jpg';
 import img4 from '../../assets/engaged/photo_2025-01-07_20-59-53.jpg';
 import wedLine from '../../assets/weedingLine.png';
+import { GuestBook } from '../component/guestbook/guestbook';
+import { Ucapan } from '../component/ucapan/ucapan';
+import { LuCalendarHeart } from 'react-icons/lu';
+import ucapanImg from '../../assets/border-ucapan.png'
 
 function Content() {
   const [backgroundImageVisible, setBackgroundImageVisible] = useState(false); 
@@ -43,9 +47,16 @@ function Content() {
     img4
   ];
 
+  const buttonUcapan = {
+      iconImage: <LuCalendarHeart />,
+      title: "Ucapan",
+      item: 10,
+      childNode: <Ucapan />,
+    }
+
   return (
     <>
-      <section className="w-[28rem] flex justify-center items-center pt-20" style={{ backgroundImage: `url(${backgroundLow})`, backgroundSize: '100%', backgroundPosition: "center" }} >
+      <section className="w-[28rem] flex justify-center items-center pt-[2rem]" style={{ backgroundImage: `url(${backgroundLow})`, backgroundSize: '100%', backgroundPosition: "center" }} >
         <div className='w-[240px] flex justify-center items-center flex-col gap-y-7 '>
           <div className='flex justify-center items-center flex-col gap-y-5 scroll-animation invisible-animation'>
             <div className='flex justify-center items-center flex-col custom-font-Ananda-normal-size text-xl scroll-animation invisible-animation' >
@@ -78,12 +89,12 @@ function Content() {
           </div>
         </div>
       </section>
-      <section className="w-[28rem] flex flex-col items-center" style={{ backgroundImage: `url(${backgroundImageVisible? backgroundLow : ""})`, backgroundSize: '100%', backgroundPosition: "center" }} >
+      <section className="w-[28rem] flex flex-col items-center pt-7" style={{ backgroundImage: `url(${backgroundImageVisible? backgroundLow : ""})`, backgroundSize: '100%', backgroundPosition: "center" }} >
         <div className='w-[240px] flex justify-center items-center flex-col gap-y-7'>
           <div className='flex justify-center items-center flex-col scroll-animation invisible-animation'>
             <p className='text-xl font-medium custom-font-cinzel'>Sabtu</p>
             <p className="text-xl font-medium custom-font-cinzel">03 May 2025</p>
-            <p className='text-[15px] custom-font-cinzel mb-3'>5 Zulkaedah 1446H</p>
+            <p className='text-[15px] custom-font-cinzel mb-7'>5 Zulkaedah 1446H</p>
             <div className='w-[13rem] scroll-animation invisible-animation'>
               <img src={wedLine}/>
             </div>
@@ -110,8 +121,8 @@ function Content() {
           </div>
         </div>
       </section>
-      <section className="w-[28rem] flex flex-col items-center" style={{ backgroundImage: `url(${backgroundImageVisible? backgroundLow : ""})`, backgroundSize: '100%', backgroundPosition: "center" }} >
-        <div className='w-[240px] flex justify-center items-center flex-col gap-y-7 pt-3'>
+      <section className="w-[28rem] flex flex-col items-center pt-7" style={{ backgroundImage: `url(${backgroundImageVisible? backgroundLow : ""})`, backgroundSize: '100%', backgroundPosition: "center" }} >
+        <div className='w-[240px] flex justify-center items-center flex-col gap-y-7'>
           <div className='flex justify-center items-center flex-col scroll-animation invisible-animation'>
             <div className='h-[200px] w-[200px] my-3'>
               <Slider imagesUrl={imageUrls}/>
@@ -123,10 +134,39 @@ function Content() {
               <Countdown/>
             </div>
           </div>
-          <div className='w-[13rem] h-[200px] scroll-animation invisible-animation'>
+          <div className='w-[13rem] scroll-animation invisible-animation'>
               <img src={wedLine}/>
           </div>
         </div>
+      </section>
+      <section className="w-[28rem] flex flex-col items-center pt-7 gap-5" style={{ backgroundImage: `url(${backgroundImageVisible? backgroundLow : ""})`, backgroundSize: '100%', backgroundPosition: "center" }} >
+        <div className='font-bold custom-font-cinzel'>GUESTBOOK</div>
+        <div className='w-[240px] flex justify-center items-center flex-col gap-y-7'>
+          <GuestBook/> 
+          <Ucapan/>
+          <div className='w-[13rem] scroll-animation invisible-animation'>
+              <img src={wedLine}/>
+          </div>
+        </div>
+      </section>
+      <section className="w-[28rem] h-[39rem] flex flex-col items-center pt-7 gap-5" style={{ backgroundImage: `url(${backgroundImageVisible? backgroundLow : ""})`, backgroundSize: '100%', backgroundPosition: "center" }} >
+          <div className='w-[240px] flex justify-center items-center flex-col'>
+            <div className='border border-black rounded-md p-5 text-lg font-base custom-font-cormorant-garamond scroll-animation invisible-animation'>
+              <div>
+                Ya Allah, berkatilah majlis perkahwinan ini, limpahkan baraqah dan rahmat kepada kedua mempelai ini, 
+              </div>
+              <div>
+                Kurniakanlah mereka zuriat yang soleh dan solehah.
+              </div>
+              <div>
+                Kekalkanlah jodoh mereka di dunia dan di akhirat dan 
+                sempurnakanlah agama mereka dengan berkat ikatan ini.
+              </div>
+            </div>
+            <div className='mt-[5rem] scroll-animation invisible-animation text-2xl custom-font-cormorant-garamond'>
+                #HazimAndYanaForever
+            </div>
+          </div>
       </section>
     </>
   )
